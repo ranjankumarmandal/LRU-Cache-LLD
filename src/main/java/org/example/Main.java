683064c286edce26bb999082346a0db36a6f1c98
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-07-01 23:01:15 +0530

    System Design: LLD: started working on LRU craation with LinkedHashMap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("System Design: LLD: LRU Cache");
+        // design LRU cache with java LLD question both by using LinkedHashMap and without of it
     }
 }
